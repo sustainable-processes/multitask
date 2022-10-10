db98f15bae10f08ab9d601699fd560e39acb0cfc
Kobi Felton <kobi.c.f@gmail.com>
2022-10-10 11:59:39 +0100

    Finish suzuki figures

--- multitask/visualization/__init__.py
+++ multitask/visualization/__init__.py
@@ -3,9 +3,11 @@ from . import cn_figures
 import typer
 
 app = typer.Typer()
-app.command()(suzuki_figures.baumgartner_suzuki_auxiliary_reizman_suzuki)
-app.command()(suzuki_figures.reizman_suzuki_auxiliary_baumgartner_suzuki)
-app.command()(suzuki_figures.reizman_suzuki_auxiliary_reizman_suzuki)
+app.command()(suzuki_figures.baumgartner_suzuki_auxiliary_one_reizman_suzuki)
+app.command()(suzuki_figures.baumgartner_suzuki_auxiliary_all_reizman_suzuki)
+app.command()(suzuki_figures.reizman_suzuki_auxiliary_one_baumgartner_suzuki)
+app.command()(suzuki_figures.reizman_suzuki_auxiliary_one_reizman_suzuki)
+app.command()(suzuki_figures.reizman_suzuki_auxiliary_all_baumgartner_suzuki)
 app.command()(suzuki_figures.all_suzuki)
 app.command()(cn_figures.baumgartner_cn_auxiliary_one_baumgartner_cn)
 app.command()(cn_figures.baumgartner_cn_auxiliary_all_baumgartner_cn)
--- multitask/visualization/suzuki_figures.py
+++ multitask/visualization/suzuki_figures.py
@@ -1,18 +1,14 @@
 """
-Make figures for publication
+Make Suzuki cross coupling figures for publication
 """
 from .plots import make_comparison_plot, get_wandb_run_dfs
 from pathlib import Path
 from summit import *
-from rdkit import Chem
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm import tqdm
-from typing import Dict, List, Optional
+from typing import List, Optional
 import wandb
 import logging
-import string
 
 
 logger = logging.getLogger(__name__)
@@ -21,7 +17,7 @@ logger = logging.getLogger(__name__)
 reizman_case_to_name = {1: "SR1", 2: "SR2", 3: "SR3", 4: "SR4"}
 
 
-def baumgartner_suzuki_auxiliary_reizman_suzuki(
+def baumgartner_suzuki_auxiliary_one_reizman_suzuki(
     num_iterations: int = 20,
     include_tags: Optional[List[str]] = None,
     filter_tags: Optional[List[str]] = None,
@@ -86,7 +82,7 @@ def baumgartner_suzuki_auxiliary_reizman_suzuki(
         )
 
         # Format subplot
-        ax.set_title(f"Aux. {reizman_case_to_name[i]}", fontsize=heading_fontsize)
+        ax.set_title(f"SB1 (Aux. {reizman_case_to_name[i]})", fontsize=heading_fontsize)
         ax.set_xlim(0, 20)
         ax.tick_params("y", labelsize=axis_fontsize)
         xlabels = np.arange(0, 21, 5)
@@ -102,14 +98,99 @@ def baumgartner_suzuki_auxiliary_reizman_suzuki(
     fig.tight_layout()
     figure_dir = Path(figure_dir)
     fig.savefig(
-        figure_dir / "baumgartner_reizman_one_cotraining_optimization.png",
+        figure_dir
+        / "baumgartner_suzuki_reizman_suzuki_one_cotraining_optimization.png",
         dpi=300,
         transparent=True,
     )
     logger.info("Plots saved to %s", figure_dir)
 
 
-def reizman_suzuki_auxiliary_baumgartner_suzuki(
+def baumgartner_suzuki_auxiliary_all_reizman_suzuki(
+    num_iterations: int = 20,
+    include_tags: Optional[List[str]] = None,
+    filter_tags: Optional[List[str]] = None,
+    only_finished_runs: bool = True,
+    wandb_entity: str = "ceb-sre",
+    wandb_project: str = "multitask",
+    figure_dir: str = "figures",
+):
+    # Wandb API
+    api = wandb.Api()
+
+    # STBO downloads
+    logger.info("Getting Baumgartner Suzuki STBO data")
+    stbo_dfs = get_wandb_run_dfs(
+        api,
+        wandb_entity=wandb_entity,
+        wandb_project=wandb_project,
+        model_name="baumgartner_suzuki",
+        strategy="STBO",
+        include_tags=include_tags,
+        filter_tags=filter_tags,
+        only_finished_runs=only_finished_runs,
+        num_iterations=num_iterations,
+    )
+    """Make plots for Baumgartner Suzuki optimization with auxiliary of Reizman Suzuki."""
+    # Setup figure
+    fig, ax = plt.subplots(1, figsize=(5, 5))
+    axis_fontsize = 16
+    heading_fontsize = 18
+    logger.info(
+        "Making plot for Baumgartner Suzuki optimization with auxiliary of Reizman Suzuki"
+    )
+    # Get MTBO data
+    logger.info(f"Getting Baumgartner Suzuki MTBO (cotrain all Reizman Suzuki) data")
+    mtbo_dfs = get_wandb_run_dfs(
+        api,
+        wandb_entity=wandb_entity,
+        wandb_project=wandb_project,
+        model_name="baumgartner_suzuki",
+        strategy="MTBO",
+        include_tags=include_tags,
+        filter_tags=filter_tags,
+        only_finished_runs=only_finished_runs,
+        num_iterations=num_iterations,
+        extra_filters={
+            "config.ct_dataset_names": [
+                f"reizman_suzuki_case_{j}" for j in range(1, 5)
+            ],
+        },
+    )
+
+    # Make comparison subplot
+    make_comparison_plot(
+        dict(results=stbo_dfs, label="STBO", color="#a50026"),
+        dict(results=mtbo_dfs, label="MTBO", color="#313695"),
+        output_name="yld_best",
+        ax=ax,
+    )
+
+    # Format subplot
+    ax.set_xlim(0, 20)
+    ax.tick_params("y", labelsize=axis_fontsize)
+    xlabels = np.arange(0, 21, 5)
+    ax.set_xticks(xlabels)
+    ax.set_xticklabels(xlabels, fontsize=axis_fontsize)
+    ax.set_ylim(0, 100)
+    k += 1
+
+    # Format and save figure
+    # fig.suptitle("Baumgartner Optimization")
+    ax.set_xlabel("Experiment number", fontsize=heading_fontsize)
+    ax.set_ylabel("Best Yield (%)", fontsize=heading_fontsize)
+    fig.tight_layout()
+    figure_dir = Path(figure_dir)
+    fig.savefig(
+        figure_dir
+        / "baumgartner_suzuki_reizman_suzuki_all_cotraining_optimization.png",
+        dpi=300,
+        transparent=True,
+    )
+    logger.info("Plots saved to %s", figure_dir)
+
+
+def reizman_suzuki_auxiliary_one_baumgartner_suzuki(
     num_iterations: int = 20,
     include_tags: Optional[List[str]] = None,
     filter_tags: Optional[List[str]] = None,
@@ -198,11 +279,14 @@ def reizman_suzuki_auxiliary_baumgartner_suzuki(
     fig.tight_layout()
     figure_dir = Path(figure_dir)
     fig.savefig(
-        figure_dir / "reizman_baumgartner_one_cotraining_optimization.png", dpi=300
+        figure_dir
+        / "reizman_suzuki_baumgartner_suzuki_one_cotraining_optimization.png",
+        dpi=300,
     )
+    logger.info("Plots saved to %s", figure_dir)
 
 
-def reizman_suzuki_auxiliary_reizman_suzuki(
+def reizman_suzuki_auxiliary_one_reizman_suzuki(
     num_iterations: int = 20,
     include_tags: Optional[List[str]] = None,
     filter_tags: Optional[List[str]] = None,
@@ -290,7 +374,113 @@ def reizman_suzuki_auxiliary_reizman_suzuki(
     fig.supylabel("Best Yield (%)", fontsize=heading_fontsize)
     fig.tight_layout()
     figure_dir = Path(figure_dir)
-    fig.savefig(figure_dir / "reizman_reizman_one_cotraining_optimization.png", dpi=300)
+    fig.savefig(
+        figure_dir / "reizman_suzuki_reizman_suzuki_one_cotraining_optimization.png",
+        dpi=300,
+    )
+    logger.info("Plots saved to %s", figure_dir)
+
+
+def reizman_suzuki_auxiliary_all_baumgartner_suzuki(
+    num_iterations: int = 20,
+    include_tags: Optional[List[str]] = None,
+    filter_tags: Optional[List[str]] = None,
+    only_finished_runs: bool = True,
+    wandb_entity: str = "ceb-sre",
+    wandb_project: str = "multitask",
+    figure_dir: str = "figures",
+):
+    """Make plots for Reizman Suzuki optimization with auxiliary of all Reizman Suzuki"""
+    # Wandb API
+    api = wandb.Api()
+
+    # Setup figure
+    fig = plt.figure(figsize=(15, 5))
+    fig.subplots_adjust(wspace=0.2, hspace=0.5)
+    k = 1
+    axis_fontsize = 14
+    heading_fontsize = 18
+
+    # Make subplots for each case
+    # letters = ["a", "b", "c", "d"]
+    logger.info(
+        "Making plots for Reizman Suzuki optimization with auxiliary of all REizman Suzuki"
+    )
+    for i in range(1, 5):
+        # STBO data
+        logger.info(f"Getting Reizman Suzuki case {i} STBO data")
+        stbo_dfs = get_wandb_run_dfs(
+            api,
+            wandb_entity=wandb_entity,
+            wandb_project=wandb_project,
+            model_name=f"reizman_suzuki_case_{i}",
+            strategy="STBO",
+            include_tags=include_tags,
+            filter_tags=filter_tags,
+            only_finished_runs=only_finished_runs,
+            num_iterations=num_iterations,
+        )
+
+        # MTBO data
+        logger.info(
+            f"Getting Reizman Suzuki case {i} (auxiliary all Reizman suzuki) MTBO data"
+        )
+        mtbo_dfs = get_wandb_run_dfs(
+            api,
+            wandb_entity=wandb_entity,
+            wandb_project=wandb_project,
+            model_name=f"reizman_suzuki_case_{i}",
+            strategy="MTBO",
+            include_tags=include_tags,
+            filter_tags=filter_tags,
+            only_finished_runs=only_finished_runs,
+            num_iterations=num_iterations,
+            extra_filters={
+                "config.ct_dataset_names": [
+                    f"reizman_suzuki_case_{j}" for j in range(1, 5) if j != i
+                ],
+            },
+        )
+
+        logger.info(
+            f"Found {len(stbo_dfs)} STBO and {len(mtbo_dfs)} MTBO runs for Reizman Suzuki case {i} with auxiliary of Baumgarnter Suzuki",
+        )
+
+        # Make subplot
+        ax = fig.add_subplot(1, 4, k)
+        make_comparison_plot(
+            dict(results=stbo_dfs, label="STBO", color="#a50026"),
+            dict(results=mtbo_dfs, label="MTBO", color="#313695"),
+            output_name="yld_best",
+            ax=ax,
+        )
+
+        # Format subplot
+        aux_names = ",".join(
+            [f"{reizman_case_to_name[j]}" for j in range(1, 5) if j != i]
+        )
+        ax.set_title(
+            f"{reizman_case_to_name[i]} (Aux. {aux_names})", fontsize=heading_fontsize
+        )
+        xlabels = np.arange(0, 21, 5)
+        ax.set_xticks(xlabels)
+        ax.set_xticklabels(xlabels, fontsize=axis_fontsize)
+        ax.set_yticks([0, 20, 40, 60, 80, 100, 120])
+        ax.set_yticklabels([0, 20, 40, 60, 80, 100, ""], fontsize=axis_fontsize)
+        ax.tick_params(direction="in")
+        k += 1
+
+    # Format and save figure
+    # fig.suptitle("Reizman Optimization", fontsize=21)
+    fig.supxlabel("Experiment number", fontsize=heading_fontsize)
+    fig.supylabel("Best Yield (%)", fontsize=heading_fontsize)
+    fig.tight_layout()
+    figure_dir = Path(figure_dir)
+    fig.savefig(
+        figure_dir / "reizman_suzuki_reizman_suzuki_all_cotraining_optimization.png",
+        dpi=300,
+    )
+    logger.info("Plots saved to %s", figure_dir)
 
 
 def all_suzuki(
@@ -303,7 +493,7 @@ def all_suzuki(
     figure_dir: str = "figures",
 ):
     """Make plots for all Suzuki optimization runs."""
-    baumgartner_suzuki_auxiliary_reizman_suzuki(
+    baumgartner_suzuki_auxiliary_one_reizman_suzuki(
         num_iterations=num_iterations,
         include_tags=include_tags,
         filter_tags=filter_tags,
@@ -312,7 +502,7 @@ def all_suzuki(
         wandb_project=wandb_project,
         figure_dir=figure_dir,
     )
-    reizman_suzuki_auxiliary_reizman_suzuki(
+    baumgartner_suzuki_auxiliary_all_reizman_suzuki(
         num_iterations=num_iterations,
         include_tags=include_tags,
         filter_tags=filter_tags,
@@ -321,7 +511,25 @@ def all_suzuki(
         wandb_project=wandb_project,
         figure_dir=figure_dir,
     )
-    reizman_suzuki_auxiliary_baumgartner_suzuki(
+    reizman_suzuki_auxiliary_one_reizman_suzuki(
+        num_iterations=num_iterations,
+        include_tags=include_tags,
+        filter_tags=filter_tags,
+        only_finished_runs=only_finished_runs,
+        wandb_entity=wandb_entity,
+        wandb_project=wandb_project,
+        figure_dir=figure_dir,
+    )
+    reizman_suzuki_auxiliary_one_baumgartner_suzuki(
+        num_iterations=num_iterations,
+        include_tags=include_tags,
+        filter_tags=filter_tags,
+        only_finished_runs=only_finished_runs,
+        wandb_entity=wandb_entity,
+        wandb_project=wandb_project,
+        figure_dir=figure_dir,
+    )
+    reizman_suzuki_auxiliary_all_baumgartner_suzuki(
         num_iterations=num_iterations,
         include_tags=include_tags,
         filter_tags=filter_tags,
